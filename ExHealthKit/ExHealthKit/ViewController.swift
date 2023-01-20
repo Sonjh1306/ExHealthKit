469fedf3601246eedc250ea3a4a06b2ddf5bc95c
Sonjh1306 <yale1306@gmail.com>
2023-01-20 15:40:58 +0900

    feat: ViewModel 생성 및 걸음 수 데이터 바인딩

--- ExHealthKit/ExHealthKit/ViewController.swift
+++ ExHealthKit/ExHealthKit/ViewController.swift
@@ -7,86 +7,44 @@
 
 import UIKit
 import SnapKit
-import HealthKit
+
 import RxSwift
 
 class ViewController: UIViewController {
     
-    
-    let healthStore = HKHealthStore()
     var disposeBag = DisposeBag()
     
     let heathKitView = HeathKitView()
+    let viewModel = ViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view = heathKitView
-        accessStepCount()
+        bind()
     }
     
-    func accessStepCount() {
-        let healthKitTypes: Set = [HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!]
+    private func bind() {
+        viewModel.input.onLoad.onNext(())
+        
+        viewModel.output.stepCount
+            .bind(to: heathKitView.todayStepsLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        viewModel.output.errorMessage
+            .bind { [weak self] (message) in
+                self?.alert(message: message)
+            }.disposed(by: disposeBag)
+    }
+   
+}
 
-        // 디바이스에서 HealthKit 사용 가능 여부 확인
-        if HKHealthStore.isHealthDataAvailable() {
-            // 권한 요청 화면 팝업 여부(처음 1회만)
-            healthStore.requestAuthorization(toShare: nil, read: healthKitTypes) { (success, error) in
-                if error != nil {
-                    print(error!.localizedDescription)
-                } else {
-                    if success {
-                        self.getSteps { (result) in
-                            DispatchQueue.main.async { [self] in
-                                let steps = String(result)
-                                heathKitView.todayStepsLabel.text = steps
-                            }
-                        }
-                    } else {
-                        print("not popup authorization view")
-                    }
-                }
-            }
-        } else {
-            print("")
-        }
+extension UIViewController {
+    func alert(title : String = "알림", message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
-    }
-    
-    private func getSteps(completion: @escaping (Double) -> Void) {
-        let type = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        alert.addAction(okAction)
         
-        let now = Date()
-        let startOfDay = Calendar.current.startOfDay(for: now)
-        var interval = DateComponents()
-        interval.day = 1
-        
-        let query = HKStatisticsCollectionQuery(quantityType: type,
-                                                quantitySamplePredicate: nil,
-                                                options: [.cumulativeSum],
-                                                anchorDate: startOfDay,
-                                                intervalComponents: interval)
-        query.initialResultsHandler = { _, result, error in
-            var resultCount = 0.0
-            result!.enumerateStatistics(from: startOfDay, to: now, with: { statistics, _ in
-                if let sum = statistics.sumQuantity() {
-                    resultCount = sum.doubleValue(for: HKUnit.count())
-                }
-                
-                DispatchQueue.main.async {
-                    completion(resultCount)
-                }
-            })
-        }
-        
-        query.statisticsUpdateHandler = { query, statistics, statisticsCollection, error in
-            if let sum = statistics?.sumQuantity() {
-                let resultCount = sum.doubleValue(for: HKUnit.count())
-                
-                DispatchQueue.main.async {
-                    completion(resultCount)
-                }
-            }
-        }
-        healthStore.execute(query)
+        present(alert, animated: true, completion: nil)
     }
 }
--- ExHealthKit/ExHealthKit/ViewModel.swift
+++ ExHealthKit/ExHealthKit/ViewModel.swift
@@ -6,3 +6,120 @@
 //
 
 import Foundation
+import HealthKit
+import RxSwift
+import RxCocoa
+
+protocol DefaultViewModelProtocol {
+    associatedtype Input
+    associatedtype Output
+    
+    var input: Input { get }
+    var output: Output { get }
+    var disposeBag: DisposeBag { get set }
+}
+
+enum HealthKitError: String, Error {
+    case notRequestAuthorization = "Not Pop Up Authorization View"
+    case canNotGetSteps = "Can't Get Steps"
+    case canNotUseHealthKit = "Can't Use HealthKit"
+}
+
+final class ViewModel: DefaultViewModelProtocol {
+    
+    struct Input {
+        let onLoad: PublishSubject<Void> = PublishSubject<Void>()
+    }
+    
+    struct Output {
+        let stepCount: PublishRelay<String> = PublishRelay<String>()
+        let errorMessage: PublishRelay<String> = PublishRelay<String>()
+    }
+    
+    var input: Input = Input()
+    var output: Output = Output()
+    
+    var disposeBag: DisposeBag = DisposeBag()
+    
+    let healthStore = HKHealthStore()
+    
+    init() {
+        input.onLoad
+            .flatMap{ _ in self.accessStepCount()}
+            .bind { [weak self] (result) in
+                switch result {
+                case .success(let result):
+                    self?.output.stepCount.accept(result)
+                case .failure(let error):
+                    self?.output.errorMessage.accept(error.rawValue)
+                }
+            }.disposed(by: disposeBag)
+    }
+    
+    private func accessStepCount() -> Observable<Result<String, HealthKitError>> {
+
+        return Observable.create { (observer) in
+            let healthKitTypes: Set = [HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!]
+
+            // 디바이스에서 HealthKit 사용 가능 여부 확인
+            if HKHealthStore.isHealthDataAvailable() {
+                // 권한 요청 화면 팝업 여부(처음 1회만)
+                self.healthStore.requestAuthorization(toShare: nil, read: healthKitTypes) { (success, error) in
+                    if error != nil {
+                        observer.onNext(.failure(.notRequestAuthorization))
+                    } else {
+                        if success {
+                            self.getSteps { (result) in
+                                observer.onNext(.success(String(result)))
+                            }
+                        } else {
+                            observer.onNext(.failure(.canNotGetSteps))
+                        }
+                    }
+                }
+            } else {
+                observer.onNext(.failure(.canNotUseHealthKit))
+            }
+            return Disposables.create()
+        }
+        
+    }
+    
+    private func getSteps(completion: @escaping (Double) -> Void) {
+        let type = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+        
+        let now = Date()
+        let startOfDay = Calendar.current.startOfDay(for: now)
+        var interval = DateComponents()
+        interval.day = 1
+        
+        let query = HKStatisticsCollectionQuery(quantityType: type,
+                                                quantitySamplePredicate: nil,
+                                                options: [.cumulativeSum],
+                                                anchorDate: startOfDay,
+                                                intervalComponents: interval)
+        query.initialResultsHandler = { _, result, error in
+            var resultCount = 0.0
+            result!.enumerateStatistics(from: startOfDay, to: now, with: { statistics, _ in
+                if let sum = statistics.sumQuantity() {
+                    resultCount = sum.doubleValue(for: HKUnit.count())
+                }
+                
+                DispatchQueue.main.async {
+                    completion(resultCount)
+                }
+            })
+        }
+        
+        query.statisticsUpdateHandler = { query, statistics, statisticsCollection, error in
+            if let sum = statistics?.sumQuantity() {
+                let resultCount = sum.doubleValue(for: HKUnit.count())
+                
+                DispatchQueue.main.async {
+                    completion(resultCount)
+                }
+            }
+        }
+        healthStore.execute(query)
+    }
+}
